7ab6b0b044020c500c271251339aed6d58dbc6c6
Karol Maciaszek <karol.maciaszek@contractors.roche.com>
2016-07-13 16:34:23 +0200

    Cluster

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 'use strict';
 
 
-const q = require('q');
+const q = require('q'),
+	cluster = require('cluster'),
+	os = require('os'),
+	config = require('config');
 
 
 const webserver = require('./backend/webserver/webserver'),
 	logger = require('./backend/service/logger');
 
 
-q.all([
-	webserver.init()
-]).done(
-	() => logger.info('[main] Service initialized'),
-	(error) => {
-		logger.error('[main] Error during initialization: %s', error.stack);
-		process.exit(1);
+if (cluster.isMaster) {
+	// determine how many nodes will be spawned
+	const childsNo = config.cluster.childs == 'auto'
+		? os.cpus().length
+		: config.cluster.childs;
+
+	// spawn nodes
+	for (let i = 0; i < childsNo; i++) {
+		cluster.fork();
 	}
-);
+
+	logger.info('[main] Child nodes spawned');
+} else {
+	// child node code
+
+	q.all([
+		webserver.init()
+	]).done(
+		() => logger.info('[main] Service initialized'),
+		(error) => {
+			logger.error('[main] Error during initialization: %s', error.stack);
+			process.exit(1);
+		}
+	);
+}
